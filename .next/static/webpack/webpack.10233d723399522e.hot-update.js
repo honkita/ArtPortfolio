4cf317bbbebbf71742de6f805e149122b32df156
Elite Lu <elitelulww@gmail.com>
2023-10-13 01:17:01 -0400

    Added placeholder text

--- .next/static/webpack/webpack.10233d723399522e.hot-update.js
+++ .next/static/webpack/webpack.10233d723399522e.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "495d92e18db97f71"; }
+/******/ 	__webpack_require__.h = function() { return "d60d266736399405"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,8 @@ import Head from "next/head";
 import { useTheme, ThemeProvider } from "next-themes";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import Layout from "./Layout";
+import utilStyles from "../styles/theme.util.module.css";
+import style from "../styles/Home.module.css";
 
 export default function Home() {
   const { resolvedTheme, setTheme } = useTheme();
@@ -16,6 +18,12 @@ export default function Home() {
           <meta name="description" content="Elite Lu's Art Portfolio Site" />
           <meta name="og:title" content={name} />
         </Head>
+        <section className={style.container}>
+          <section className={utilStyles.heading2Xl}>Elite Lu</section>
+          <section className={utilStyles.headingMD}>
+            Still working on this come back soon for my art stuff!!!!!
+          </section>
+        </section>
       </div>
     </Layout>
   );
