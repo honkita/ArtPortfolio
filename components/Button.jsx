fa122f945e82e01f85e921330b1ecf0c2a71e344
Elite Lu <elitelulww@gmail.com>
2024-06-04 14:46:08 -0400

    Fixed image loading issue and added fade animation

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,33 +2,33 @@ import React, { useEffect, useState } from "react";
 import utilStyles from "../styles/theme.util.module.css";
 import style from "../styles/Home.module.css";
 import Link from "next/link";
-import Head from "next/head";
 import IconsLinks from "../public/IconsLinks.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Button(props) {
+  const {
+    library,
+    config,
+    findIconDefinition,
+  } = require("@fortawesome/fontawesome-svg-core");
   var IconsLinksJSON = JSON.parse(JSON.stringify(IconsLinks));
+
   function iconPicker() {
-    if (IconsLinksJSON[props.image] != null) return IconsLinksJSON[props.image];
-    return null;
+    console.log(IconsLinksJSON[props.image]["iconName"]);
+    return findIconDefinition({
+      prefix: String(IconsLinksJSON[props.image]["prefix"]),
+      iconName: String(IconsLinksJSON[props.image]["iconName"]),
+    });
   }
 
   return (
     <Link href={props.url} target={"_blank"}>
-      <Head>
-        <script
-          // you might need to get a newer version
-          src="https://kit.fontawesome.com/fbadad80a0.js"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
       <button id={props.name} className={style.button}>
         <section className={style.horizontal}>
           <div className={style.logoPlacement}>
-            {
-              <span className={`${utilStyles.logoImage} `} priority>
-                <i class={iconPicker()} alt={props.name}></i>
-              </span>
-            }
+            <div className={`${utilStyles.logoImage} `} priority>
+              <FontAwesomeIcon icon={iconPicker()} />
+            </div>
           </div>
           <div className={style.textPlacement}>
             <div className={utilStyles.headingMd}>{props.name}</div>
